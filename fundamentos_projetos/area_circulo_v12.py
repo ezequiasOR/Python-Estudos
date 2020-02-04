2e4810148cc97918bfad3e45ec5026435e2aaa91
Ezequias Rocha <ezequias.rocha@ccc.ufcg.edu.br>
2020-02-04 14:48:11 -0300

    fundamentos: update 1

--- fundamentos_projetos/area_circulo_v12.py
+++ fundamentos_projetos/area_circulo_v12.py
@@ -3,14 +3,18 @@ from math import pi
 import sys
 
 
+def help():
+    print('É necessário informar a área do círculo.')
+    print('Sintaxe: {} <raio>'.format(sys.argv[0][2:]))
+
+
 def circulo(raio):
     return pi * float(raio) ** 2
 
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print('É necessário informar a área do círculo.')
-        print('Sintaxe: {} <raio>'.format(sys.argv[0][2:]))
+        help()
     else:
         raio = sys.argv[1]
         area = circulo(raio)
--- fundamentos_projetos/area_circulo_v13.py
+++ fundamentos_projetos/area_circulo_v13.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
 from math import pi
 import sys
+import errno
+
+
+def help():
+    print('É necessário informar a área do círculo.')
+    print('Sintaxe: {} <raio>'.format(sys.argv[0][2:]))
 
 
 def circulo(raio):
@@ -9,8 +15,8 @@ def circulo(raio):
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
-        print('É necessário informar a área do círculo.')
-        print('Sintaxe: {} <raio>'.format(sys.argv[0][2:]))
+        help()
+        sys.exit(errno.EPERM)
     else:
         raio = sys.argv[1]
         area = circulo(raio)
