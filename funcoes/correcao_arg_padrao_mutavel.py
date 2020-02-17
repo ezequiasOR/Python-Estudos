f378adc29df6adeafc89a8ae399ccc5fc6b1058e
Ezequias Rocha <ezequias.rocha@ccc.ufcg.edu.br>
2020-02-17 19:28:26 -0300

    funções: update 3

---
+++ funcoes/correcao_arg_padrao_mutavel.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fibonacci(sequencia=None):
+    sequencia = sequencia or [0, 1]
+    sequencia.append(sequencia[-1] + sequencia[-2])
+    return sequencia
+
+
+if __name__ == "__main__":
+    inicio = fibonacci()
+    print(inicio, id(inicio))
+    print(fibonacci(inicio))
+    restart = fibonacci()
+    print(restart, id(restart))
---
+++ funcoes/problema_arg_padrao_mutavel.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def fibonacci(sequencia=[0, 1]):
+    # Uso de mutáveis como valor default (armadilha)
+    sequencia.append(sequencia[-1] + sequencia[-2])
+    return sequencia
+
+
+if __name__ == "__main__":
+    inicio = fibonacci()
+    print(inicio, id(inicio))
+    print(fibonacci(inicio))
+    restart = fibonacci()
+    print(restart, id(restart))
