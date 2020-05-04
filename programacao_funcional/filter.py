f8a13a6d4aba18d138523719c376530662a24ef3
Ezequias Rocha <ezequias.rocha@ccc.ufcg.edu.br>
2020-05-04 09:44:15 -0300

    funcional: update 1

---
+++ programacao_funcional/filter.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+pessoas = [
+    {'nome': 'Pedro', 'idade': 11},
+    {'nome': 'Mariana', 'idade': 18},
+    {'nome': 'Arthur', 'idade': 26},
+    {'nome': 'Rebeca', 'idade': 6},
+    {'nome': 'Thiago', 'idade': 19},
+    {'nome': 'Gabriela', 'idade': 17},
+]
+
+menores = filter(lambda p: p['idade'] < 18, pessoas)
+print(list(menores))
+
+nomes_grandes = filter(lambda p: len(p['nome']) >= 7, pessoas)
+print(list(nomes_grandes))
