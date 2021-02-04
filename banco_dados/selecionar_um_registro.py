10f0d89cc59394e4b3012b1615e2817dd25467f8
Ezequias Rocha <ezequias.rocha@ccc.ufcg.edu.br>
2021-02-04 11:23:21 -0300

    banco de dados

---
+++ banco_dados/selecionar_um_registro.py
@@ -0,0 +1,15 @@
+from bd import nova_conexao
+
+sql = 'SELECT tel, nome FROM contatos LIMIT 3 OFFSET 2'
+
+with nova_conexao() as conexao:
+		cursor = conexao.cursor()
+		cursor.execute(sql)
+
+		print(cursor.fetchone())
+		print(cursor.fetchone())
+		print(cursor.fetchone())
+		print(cursor.fetchone())
+		print(cursor.fetchone())
+		print(cursor.fetchone())
+		print(cursor.fetchone())
